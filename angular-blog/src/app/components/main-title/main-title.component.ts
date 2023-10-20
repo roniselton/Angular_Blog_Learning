b655de60b25618aa5aca1f6fe79704a9fec1572d
Roniselton <ronibarreto@gmail.com>
2023-10-20 15:50:22 -0300

    components

---
+++ angular-blog/src/app/components/main-title/main-title.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-title',
+  templateUrl: './main-title.component.html',
+  styleUrls: ['./main-title.component.css']
+})
+export class MainTitleComponent {
+
+}
